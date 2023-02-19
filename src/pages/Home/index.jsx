b481e76841602429f17f0fb929e716aff9eff139
lssouzadev <leoss91@hotmail.com>
2023-02-19 18:58:47 -0300

    Marcar e desmarcar tags(filtro)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { FiPlus } from 'react-icons/fi'
+import { useState, useEffect } from 'react'
 
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
@@ -6,7 +7,30 @@ import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
+import { api } from '../../services/api'
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter((tag) => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      setTagsSelected((prevState) => [...prevState, tagName])
+    }
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/tags')
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
   return (
     <Container>
       <Brand>
@@ -17,14 +41,18 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" />
+          <ButtonText title="Todos" isActive={tagsSelected.length === 0} />
         </li>
-        <li>
-          <ButtonText title="Nodejs" />
-        </li>
-        <li>
-          <ButtonText title="React" />
-        </li>
+        {tags &&
+          tags.map((tag, index) => (
+            <li key={String(index)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))}
       </Menu>
 
       <Search>
