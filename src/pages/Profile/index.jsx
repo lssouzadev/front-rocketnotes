b51625262d6d1fea336f0f903e298a88b7713a70
lssouzadev <leoss91@hotmail.com>
2023-02-21 16:10:57 -0300

    Finalizado

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,13 @@
 import { Container, Form, Avatar } from './styles'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { useState } from 'react'
 import { useAuth } from '../../hooks/auth'
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 import { api } from '../../services/api'
+import { useNavigate } from 'react-router-dom'
 
 export function Profile() {
   const { user, updateProfile } = useAuth()
@@ -22,16 +23,17 @@ export function Profile() {
 
   const [avatar, setAvatar] = useState(avatarUrl)
   const [avatarFile, setAvatarFile] = useState(null)
+  const navigate = useNavigate()
 
   async function handleUpdate() {
-    const user = {
+    const updated = {
       name,
       email,
       password: passwordNew,
       old_password: passwordOld,
     }
-
-    await updateProfile({ user, avatarFile })
+    const userUpdated = Object.assign(user, updated)
+    await updateProfile({ user: userUpdated, avatarFile })
   }
 
   function handleChangeAvatar(event) {
@@ -42,12 +44,16 @@ export function Profile() {
     setAvatar(imagePreview)
   }
 
+  function handleBack() {
+    navigate(-1)
+  }
+
   return (
     <Container>
       <header>
-        <Link to="/">
+        <button type="button" onClick={handleBack}>
           <FiArrowLeft />
-        </Link>
+        </button>
       </header>
 
       <Form>
